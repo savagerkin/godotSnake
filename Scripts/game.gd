14d71a90645e9843c4ddcf0624c5caab33d06c73
savagerkin <erkin.uyguroglu@gmail.com>
2026-07-09 01:02:51 +0200

    Snakey snakey

---
+++ Scripts/game.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var score: int
+var game_started: bool = false
+
+@export var snake_scene : PackedScene
+
+var cell_size: int = 50
+var cells: int = 20
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
