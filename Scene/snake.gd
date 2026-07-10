032664ef156d04f83f56243a15f3566f01e38cb4
savagerkin <erkin.uyguroglu@gmail.com>
2026-07-10 20:05:59 +0200

    Restarted fully, doing my own thing

---
+++ Scene/snake.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+
+var direction := Vector2.ZERO	
+
+func _process(delta: float) -> void:
+	
+	#Check which button has pressed last, and make the direction equal to it
+	if Input.is_action_just_pressed("move_up"):
+		direction = Vector2.UP
+	elif Input.is_action_just_pressed("move_down"):
+		direction = Vector2.DOWN
+	elif Input.is_action_just_pressed("move_left"):
+		direction = Vector2.LEFT
+	elif Input.is_action_just_pressed("move_right"):
+		direction = Vector2.RIGHT
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	position += direction * 100.0 * delta
