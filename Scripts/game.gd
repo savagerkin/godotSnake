323753614f501ad944e1e77f588264dec642f21b
savagerkin <erkin.uyguroglu@gmail.com>
2026-07-11 11:51:44 +0200

    Snakey snakey can eat

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,23 +1,46 @@
 extends Node2D
 
 var game_started: bool = false
+var last_score: int = 0
 
 @export var score_label: Label
 @export var beer_scene: PackedScene
 
-
+# Start the game, initialization
 func _ready() -> void:
 	_start_game()
 
 
+func _process(_delta: float) -> void:
+	# Check if the score has changed if so ->
+	if last_score != ScoreManager.get_score():
+		last_score = ScoreManager.get_score()
+		# Update the score in the  label
+		_update_score_label()
+		# Spawn a new beer
+		_spawn_beer()
+
+# Initialization for the scores, beers and points.
 func _start_game() -> void:
 	ScoreManager.set_score(0)
-	_update_score_label()
+	last_score = 0
 	game_started = true
 
-func _process(delta: float) -> void:
 	_update_score_label()
+	_spawn_beer()
 
+# A funciton to create the beer, between a random range
+func _spawn_beer() -> void:
+	var beer := beer_scene.instantiate()
+	add_child(beer)
+	# So it dosent spill through the screen.
+	var margin := 10.0
 
+	beer.position = Vector2(
+		randf_range(-100.0 + margin, 100.0 - margin),
+		randf_range(-100.0 + margin, 100.0 - margin)
+	)
+
+#Function updates the score label.
 func _update_score_label() -> void:
 	score_label.text = "Score: " + str(ScoreManager.get_score())
--- Scripts/snake.gd
+++ Scripts/snake.gd
@@ -1,19 +1,20 @@
 extends CharacterBody2D
 
-var direction := Vector2.ZERO	
+var direction := Vector2.ZERO
+var speed := 100.0
 
-func _process(delta: float) -> void:
-	
-	#Check which button has pressed last, and make the direction equal to it
-	if Input.is_action_just_pressed("move_up") && direction != Vector2.DOWN :
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("move_up") and direction != Vector2.DOWN:
 		direction = Vector2.UP
-	elif Input.is_action_just_pressed("move_down") && direction != Vector2.UP:
+	elif Input.is_action_just_pressed("move_down") and direction != Vector2.UP:
 		direction = Vector2.DOWN
-	elif Input.is_action_just_pressed("move_left") && direction != Vector2.RIGHT:
+	elif Input.is_action_just_pressed("move_left") and direction != Vector2.RIGHT:
 		direction = Vector2.LEFT
-	elif Input.is_action_just_pressed("move_right") && direction != Vector2.LEFT:
+	elif Input.is_action_just_pressed("move_right") and direction != Vector2.LEFT:
 		direction = Vector2.RIGHT
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
-	position += direction * 100.0 * delta
+
+func _physics_process(_delta: float) -> void:
+	velocity = direction * speed
+	move_and_slide()
